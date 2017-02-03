42cd204c764f59a964b13d009253a67dff63b65a
Adriano João da Silva <adrianojoao99@gmail.com>
2017-02-03 00:54:42 -0300

    Criação de tabelas - Teste
    Existe alguns pontos a serem vistos como:
    . Salvar dado como array (Pesquisar na documentação);
    . Ver por que não está sendo possível criar relação entre Disciplina e
    Curso;

--- public/js/caderno-on-line.js
+++ public/js/caderno-on-line.js
@@ -11,6 +11,8 @@ Parse.Object.registerSubclass('Curso', Curso);
 Parse.Object.registerSubclass('Disciplina', Disciplina);
 Parse.Object.registerSubclass('Anotacao', Anotacao);
 
+console.log("Começou Instituicao salvando");
+
 var instituicao = new Instituicao();
 instituicao.set("nome", 'Facol');
 instituicao.set("descricao", 'Teste test teste');
@@ -22,6 +24,8 @@ instituicao.save(null, {
     alert('Failed to create new object, with error code: ' + error.message);
   }
 });
+
+console.log("Próximo é Curso");
 
 var curso = new Curso();
 curso.set("nome", 'Sistemas de Informação');
@@ -37,14 +41,37 @@ curso.save(null, {
   }
 });
 
+console.log("Proximo é Disciplina");
 
 var disciplina = new Disciplina();
 disciplina.set("nome", 'Jogos');
 disciplina.set("nomeProfessor", 'Chicout');
 disciplina.set("descricao", 'Teste test teste');
-disciplina.set("diasAula", ["seg", "ter"]);
+disciplina.set("diasAula", 'qua'); // NÃO FUNCIONOU COMO ARRAY - ERRO 400 BAD REQUET
+//disciplina.set("curso", curso); NÃO CONSEGUE CRIAR A RELAÇÃO COM 'CURSO' - VER DEPOIS O PROBLEMA... 
+disciplina.save(null, {
+  success: function(obj) {
+    alert('New object created with objectId: ' + obj.id);
+  },
+  error: function(obj, error) {
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
+});
+
+console.log("Próximo é Anotação");
 
 var anotacao = new Anotacao();
 anotacao.set("assunto", 'Jogos Antigos');
 anotacao.set("texto", 'Teste test teste');
 anotacao.set("data", Date.now());
+anotacao.set("disciplina", disciplina);
+anotacao.save(null, {
+  success: function(obj) {
+    alert('New object created with objectId: ' + obj.id);
+  },
+  error: function(obj, error) {
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
+});
+
+console.log("Final do Script");
