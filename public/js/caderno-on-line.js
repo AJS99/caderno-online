7a62b01b959aba9871fdf2d5bce43595e9f0ffed
Adriano João da Silva <adrianojoao99@gmail.com>
2017-02-14 22:50:39 -0300

    teste login

--- public/js/caderno-on-line.js
+++ public/js/caderno-on-line.js
@@ -2,20 +2,21 @@ Parse.initialize("caderno7259");
 Parse.serverURL = 'http://caderno-on-line.herokuapp.com/parse';
 
 var Instituicao = Parse.Object.extend("Instituicao");
-var Curso = Parse.Object.extend("Curso");
 var Disciplina = Parse.Object.extend("Disciplina");
 var Anotacao = Parse.Object.extend("Anotacao");
+var Curso = Parse.Object.extend("Curso");
 
 Parse.Object.registerSubclass('Instituicao', Instituicao);
-Parse.Object.registerSubclass('Curso', Curso);
 Parse.Object.registerSubclass('Disciplina', Disciplina);
 Parse.Object.registerSubclass('Anotacao', Anotacao);
+Parse.Object.registerSubclass('Curso', Curso);
 
-console.log("Começou Instituicao salvando");
+/*var data = new Date();
+console.log(data);
 
 var instituicao = new Instituicao();
-instituicao.set("nome", 'Facol');
-instituicao.set("descricao", 'Teste test teste');
+instituicao.set("nome", "Facol");
+instituicao.set("descricao", "Descrição da Instituição");
 instituicao.save(null, {
   success: function(obj) {
     alert('New object created with objectId: ' + obj.id);
@@ -25,45 +26,58 @@ instituicao.save(null, {
   }
 });
 
-console.log("Próximo é Curso");
-
 var curso = new Curso();
-curso.set("nome", 'Sistemas de Informação');
-curso.set("nomeCoordenador", 'Cleyton');
-curso.set("descricao", 'Teste test teste');
+curso.set("nome", "Sistemas de Informação 2");
+curso.set("nomeCoordenador", "Cleyton Rodrigues");
+curso.set("descricao", "Descrição do Curso");
 curso.set("instituicao", instituicao);
-curso.save(null, {
-  success: function(obj) {
+curso.save(null).then(
+  function(obj) {
     alert('New object created with objectId: ' + obj.id);
+
+    var dias_aula = ["qua"];
+    console.log(dias_aula);
+    var disciplina = new Disciplina();
+    disciplina.set("nome", "Jogos 2");
+    disciplina.set("nomeProfessor", "Chicout");
+    disciplina.set("descricao", "Descrição da Disciplina");
+    disciplina.set("idCurso", curso); 
+    disciplina.set("dias", dias_aula);
+    return disciplina.save(null);
   },
-  error: function(obj, error) {
+  function(error) {
     alert('Failed to create new object, with error code: ' + error.message);
   }
-});
+).then(
+  function(obj) {
+    alert('New object created with objectId: ' + obj.id);
+  },
+  function(error) {
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
+);
 
-console.log("Proximo é Disciplina");
-
+var dias_aula = ["qua"];
+console.log(dias_aula);
 var disciplina = new Disciplina();
-disciplina.set("nome", 'Jogos');
-disciplina.set("nomeProfessor", 'Chicout');
-disciplina.set("descricao", 'Teste test teste');
-disciplina.set("diasAula", 'qua'); // NÃO FUNCIONOU COMO ARRAY - ERRO 400 BAD REQUET
-//disciplina.set("curso", curso); NÃO CONSEGUE CRIAR A RELAÇÃO COM 'CURSO' - VER DEPOIS O PROBLEMA... 
+disciplina.set("nome", "Jogos");
+disciplina.set("nomeProfessor", "Chicout");
+disciplina.set("descricao", "Descrição da Disciplina");
+disciplina.set("idCurso", curso); 
+disciplina.set("dias", dias_aula);
 disciplina.save(null, {
   success: function(obj) {
-    alert('New object created with objectId: ' + obj.id);
+    alert('Objeto criado com o ID' + obj.id);
   },
-  error: function(obj, error) {
-    alert('Failed to create new object, with error code: ' + error.message);
+  error: function(obj,error) {
+    alert('Falha ao criar novo objeto, com código errado: ' +error.message);
   }
 });
 
-console.log("Próximo é Anotação");
-
 var anotacao = new Anotacao();
-anotacao.set("assunto", 'Jogos Antigos');
-anotacao.set("texto", 'Teste test teste');
-anotacao.set("data", Date.now());
+anotacao.set("assunto", "Introdução a Lua");
+anotacao.set("texto", "Lua é uma linguagem criada no Brasil, no Rio de Janeiro, por...");
+anotacao.set("dataAnotacao", data);
 anotacao.set("disciplina", disciplina);
 anotacao.save(null, {
   success: function(obj) {
@@ -72,6 +86,4 @@ anotacao.save(null, {
   error: function(obj, error) {
     alert('Failed to create new object, with error code: ' + error.message);
   }
-});
-
-console.log("Final do Script");
+});*/
